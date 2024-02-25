8cf0794cc0d09d0b7ad5cb368fe71820b579b5cd
James Best <jim@justjibba.net>
2024-02-25 05:34:41 +0000

    Add a system message with a description of the tables in the db

--- main.py
+++ main.py
@@ -4,16 +4,23 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage  # plain system message with no input
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
+
 prompt = ChatPromptTemplate(  # type: ignore
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to the SQLite database.\n{tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(
             variable_name="agent_scratchpad"
@@ -25,4 +32,4 @@ tools = [run_query_tool]
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
-agent_executor("How many users are in the database?")
+agent_executor("How many users have provided a shipping address?")
--- tools/sql.py
+++ tools/sql.py
@@ -4,6 +4,13 @@ from langchain.tools import Tool
 conn = sqlite3.connect("db.sqlite")
 
 
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name from sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
 def run_sqlite_query(query):
     c = conn.cursor()
     try:
